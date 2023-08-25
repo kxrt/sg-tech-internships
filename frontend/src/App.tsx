c963d3f74f7bb0e8b8ef664cb796debbdb488711
Kartikeya <kvrtikeya@gmail.com>
2023-08-25 08:18:10 +0800

    feat: add Google Analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ReactGA from "react-ga4";
 import { Header } from "./components/HeroHeader";
 import { Internships } from "./components/Internships";
 import { ModalForm } from "./components/ModalForm";
 
+ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID);
+
 function App() {
   const [openModal, setOpenModal] = useState<boolean>(false);
+
+  useMemo(() => {
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
+
+  const handleOpenModal = (value: boolean) => {
+    setOpenModal(value);
+    ReactGA.send({
+      hitType: "event",
+      eventCategory: "Click",
+      eventAction: "Open Modal",
+    });
+  };
+
   return (
     <>
-      {openModal && <ModalForm setOpenModal={setOpenModal} />}
+      {openModal && <ModalForm setOpenModal={handleOpenModal} />}
       <Header setOpenModal={setOpenModal} />
       <Internships />
       <ToastContainer
