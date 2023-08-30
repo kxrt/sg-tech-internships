1ec229c2a6394e43816cd1f098530571e0d8cf5e
Kartikeya <kvrtikeya@gmail.com>
2023-08-30 18:57:10 +0800

    feat: add last updated date

--- frontend/src/components/Internships.tsx
+++ frontend/src/components/Internships.tsx
@@ -20,19 +20,46 @@ export function Internships({
     []
   );
   const [searchQuery, setSearchQuery] = useState<string>("");
+  const [lastUpdated, setLastUpdated] = useState<string>("");
 
+  // run once on first render
   useEffect(() => {
+    // get list of internship positions
     axios.get("/api/internships").then((response) => {
       setSummerInternships(response.data.data.summer);
       setOffcycleInternships(response.data.data.offcycle);
     });
   }, []);
 
+  // set last updated date
+  useEffect(() => {
+    // prevent failure on first render
+    if (summerInternships.length == 0 || offcycleInternships.length == 0)
+      return;
+
+    // get list of dates added
+    const date: Date = summerInternships
+      .map((internship) => new Date(internship.date_added))
+      .concat(
+        offcycleInternships.map((internship) => new Date(internship.date_added))
+      )
+      // get latest update
+      .reduce((acc, date) => (acc > date ? acc : date));
+    setLastUpdated(
+      new Intl.DateTimeFormat("en-SG", {
+        month: "short",
+        day: "2-digit",
+        year: "numeric",
+      }).format(date)
+    );
+  }, [summerInternships, offcycleInternships]);
+
   return (
     <>
       <div className="top-interactive">
         <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       </div>
+      <div>Last updated: {lastUpdated}</div>
       <InternshipList
         internships={summerInternships}
         searchQuery={searchQuery}
