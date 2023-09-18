3955e0365533560ad435fba6047455231b415d65
ackselz <tanshaochong48@gmail.com>
2023-09-18 13:39:00 +0800

    refactor: shift styles to mantine button variant

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -19,8 +19,40 @@ const router = createBrowserRouter([
     },
 ]);
 
+const theme = {
+    components: {
+        Button: {
+            variants: {
+                cta: () => ({
+                    root: {
+                        borderRadius: "8px",
+                        color: "#fbfbfb",
+                        border: "1px solid transparent",
+                        padding: "0.4em, 0.8em",
+                        fontSize: "1em",
+                        fontWeight: 500,
+                        fontFamily: "inherit",
+                        backgroundColor: "#6161ff",
+                        cursor: "pointer",
+                        transition:
+                            "border-color 0.25s, background-color 0.25s",
+                        boxShadow: "0px 10px 15px rgba(97, 97, 255, 0.25)",
+                        "&:hover": {
+                            borderColor: "#646cff",
+                            backgroundColor: "#4046b7",
+                        },
+                        "&:focus, &:focus-visible": {
+                            outline: "4px auto -webkit-focus-ring-color",
+                        },
+                    },
+                }),
+            },
+        },
+    },
+};
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
         <RouterProvider router={router} />
     </MantineProvider>
 );
