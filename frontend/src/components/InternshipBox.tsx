67d834fca241eaaeb0bdb84d9e5a6baf20ae6933
ackselz <tanshaochong48@gmail.com>
2023-09-27 01:37:02 +0800

    fix: temp fix for button styles

--- frontend/src/components/InternshipBox.tsx
+++ frontend/src/components/InternshipBox.tsx
@@ -1,27 +1,33 @@
+import { Button } from "@mantine/core";
 import { Internship } from "./Internships";
+import { Link } from "react-router-dom";
 
 export function InternshipBox({ internship }: { internship: Internship }) {
-  return (
-    <>
-      <div className="internship-box-wrapper">
-        <div className="internship-box-left">
-          <p style={{ fontWeight: 600 }}>{internship.company}</p>
-          <p className="internship-role">{internship.role}</p>
-        </div>
-        <div className="internship-box-right">
-          <button disabled className="internship-date">
-            {internship.date_added.slice(0, 6)}
-          </button>
-          <button
-            className="internship-apply-button"
-            onClick={() => {
-              window.open(internship.link, "_blank");
-            }}
-          >
-            Apply
-          </button>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="internship-box-wrapper">
+                <div className="internship-box-left">
+                    <p style={{ fontWeight: 600 }}>{internship.company}</p>
+                    <p className="internship-role">{internship.role}</p>
+                </div>
+                <div className="internship-box-right">
+                    <button disabled className="internship-date">
+                        {internship.date_added.slice(0, 6)}
+                    </button>
+                    <Link
+                        to={internship.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Button
+                            variant="cta"
+                            className="internship-apply-button"
+                        >
+                            Apply
+                        </Button>
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
 }
