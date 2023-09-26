e5dcd850f054094b707ec2681c5095ee5aa10e75
ackselz <tanshaochong48@gmail.com>
2023-09-27 01:22:40 +0800

    fix: modify vite config api proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,49 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://backend:8000",
-        changeOrigin: true,
-      },
-    },
-  },
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://backend:8000",
+//         // target: "http://localhost:8000", // development
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
+
+export default defineConfig(({ command, mode }) => {
+    if (command === "serve") {
+        return {
+            plugins: [react()],
+            server: {
+                proxy: {
+                    "/api": {
+                        target:
+                            mode === "dev"
+                                ? "http://backend:8000"
+                                : "http://localhost:8080",
+                        // target: "http://localhost:8000", // development
+                        changeOrigin: true,
+                    },
+                },
+            },
+        };
+    } else {
+        // command === 'build'
+        return {
+            plugins: [react()],
+            server: {
+                proxy: {
+                    "/api": {
+                        target: "http://backend:8000",
+                        // target: "http://localhost:8000", // development
+                        changeOrigin: true,
+                    },
+                },
+            },
+        };
+    }
 });
